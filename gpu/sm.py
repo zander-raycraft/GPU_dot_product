cd192e4b6ff80d70ebcfef0d889ff07d9dc9526e
zander-raycraft <zanderraycraft504@gmail.com>
2025-02-17 09:45:53 -0600

    am busy but got the skeleton done for the gpu dir classes

--- gpu/sm.py
+++ gpu/sm.py
@@ -1,4 +1,38 @@
+import numpy as np
+from warp import warp
+
+'''
+    @breif: This is the streaming multiprocessor, this SM houses a warp which iterates over the defined number
+            ALUs. This copmutes part of the overall GPU_SIM input
+            
+    @params: warp -> CLASS:warp(), this is the collection of threads that will run simple operations in parallel
+    @params: ALU_LIST -> list, this is a list for tracking and running the ALUs in parallel
+    @params: local_mem -> int, this is the local memory where the ALU outputs are stored   
+    @params: cc -> int, control code for what operation to do on the ALU    
+
+'''
 class stream_multi():
     
-    def __init__():
+    '''
+        @param: this is the intialization funciton for the SM class
+    '''
+    def __init__(self, num_alu: int, cc: int):
+        self.warp = warp()
+        self.ALU_LIST = []
+        self.local_mem = np.zeros(512, dtype=np.int32)
+        self.cc = cc
+    
+    '''
+        @breif: This funciton sends data from the the local memory in the ALU to the global memory in the GPU
+        
+        @return: result -> np.array, this is the info from the local memory int the ALU
+    '''
+    def send_info(self) -> np.array:
+        pass
+    
+    '''
+        @breif: this function runs the calculations on the SM which controls the threads (warp) that runs
+                parallel simple operations on the ALUs
+    '''
+    def run_calculations(self):
         pass
--- gpu/warp.py
+++ gpu/warp.py
@@ -1,6 +1,39 @@
+from typing import List
 from ..logic_gates.alu import ALU
 
-class warp():
-    
-    def __init__():
+class Warp:
+    def __init__(self, num_threads: int):
+        '''
+            @brief: Initializes the warp with a specified number of threads.
+            
+            @params: num_threads -> int, the number of threads in this warp.
+            @returns: None
+        '''
+        self.num_threads = num_threads
+        self.threads = [None] * num_threads
+
+    def distribute_work(self, work_data: List):
+        '''
+            @brief: Distributes the given work data among the warp's threads.
+            
+            @params: work_data -> List, a list of data chunks or tasks for each thread.
+            @returns: None
+        '''
+        pass
+
+    def run(self, control_code: int):
+        '''
+            @brief: Executes the tasks in the warp according to the provided control code.
+            
+            @params: control_code -> int, an integer (or Enum) that indicates which operation to perform.
+            @returns: None
+        '''
+        pass
+
+    def gather_results(self) -> List:
+        '''
+            @brief: Collects and returns the results from each thread in the warp.
+            
+            @returns: List, a list of results from the warp.
+        '''
         pass
